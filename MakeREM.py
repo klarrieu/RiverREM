ee21f5da36368e7cd9c745020d72227b1000e774
Kenneth Larrieu <kglarrieu@ucdavis.edu>
2022-06-14 10:45:08 -0700

    got half-working, but needs more effort (see TODO)

--- MakeREM.py
+++ MakeREM.py
@@ -1,23 +1,37 @@
 #!/usr/bin/env python
 import os
 import sys
+import numpy as np
 import gdal
 import osr
 import ogr
 import osmnx  # for querying OpenStreetMaps data to get river centerlines
+from scipy.spatial import cKDTree as KDTree
 import subprocess
 import time
+from RasterViz import RasterViz
 
 
 start = time.time()
 
-"""An attempt to autoamtically make river REM from DEM"""
-
 
 class REMMaker(object):
+    """
+    An attempt to automatically make river REM from DEM
+
+    TODO:
+        - improve longest river identification by cropping geometries to DEM domain
+        - play with interpolation parameters to get nice smoothness
+        - use eps arg for kd-tree query to speedup?
+        - ensure when dem_array is created that nodata values are np.nan
+        - make interpolation more efficient for querying rasters with large nodata areas by only interpolating valid DEM pixels
+        - blend color-relief of REM with hillshade of original DEM
+        - use hypsographic analysis/quantiles to scale colormap range nicely? max out color ramp not too far above bank to avoid interpolation artefacts
+    """
     def __init__(self, dem):
         self.dem = dem
-        self.proj = self.get_projection()
+        self.dem_name = os.path.basename(dem).split('.')[0]
+        self.proj, self.epsg_code = self.get_projection()
         # bbox (n_lat, s_lat, e_lon, w_lon) of DEM
         self.bbox = self.get_bbox()
         self.run()
@@ -31,6 +45,7 @@ class REMMaker(object):
         if not os.path.exists(dem):
             raise FileNotFoundError(f"Cannot find input DEM: {dem}")
         self._dem = dem
+        self.get_dem_coords()
         return self._dem
 
     def get_projection(self):
@@ -38,10 +53,10 @@ class REMMaker(object):
         gtif = gdal.Open(self.dem, gdal.GA_ReadOnly)
         proj = osr.SpatialReference(wkt=gtif.GetProjection())
         epsg_code = proj.GetAttrValue('AUTHORITY', 1)
-        return epsg_code
+        return proj, epsg_code
 
     def get_bbox(self):
-        """Get extent for DEM raster."""
+        """Get lat/long extent for DEM raster."""
         src = gdal.Open(self.dem, gdal.GA_ReadOnly)
         source_crs = src.GetSpatialRef()
         ulx, xres, xskew, uly, yskew, yres = src.GetGeoTransform()
@@ -56,66 +71,194 @@ class REMMaker(object):
         return bbox
 
     def get_river_centerline(self):
-        """Find river centerline within DEM area using OSM Ways"""
+        """Find centerline of river(s) within DEM area using OSM Ways"""
         print("\nFinding river centerline.")
         # get OSM Ways within bbox of DEM (returns geopandas geodataframe)
         self.ways = osmnx.geometries_from_bbox(*self.bbox, tags={'waterway': ['river', 'stream', 'tidal channel']})
         self.ways = self.ways.dropna(subset=['name'])
-        self.rivers = []
-        for i in range(len(self.ways)):
-            print(i)
-            osmid = "W" + str(self.ways.index[i][1])
-            river = osmnx.geocode_to_gdf(osmid, by_osmid=True).to_crs(epsg=self.proj)
-            river.name = self.ways.iloc[i]['name']
-            self.rivers.append(river)
-        print(f"Found river(s): {', '.join(set(self.ways['name']))}")
+        # get all natural waterways in domain
+        osmids = ["W" + str(i[1]) for i in self.ways.index]
+        names = self.ways.name.values
+        # read into geodataframe with same CRS as DEM
+        self.rivers = osmnx.geocode_to_gdf(osmids, by_osmid=True).to_crs(epsg=self.epsg_code)
+        # carry over name attribute from OSM ways
+        self.rivers['river_name'] = names
+        # get unique names
+        river_names = set(names)
+        self.river_ids = {name: i + 1 for i, name in enumerate(river_names)}
+        self.rivers['river_id'] = [self.river_ids[name] for name in names]
+        print(f"Found river(s): {', '.join(river_names)}")
+        # find river with greatest length (sum of all segments with same name)
+        """
+        print("\nRiver lengths:")
+        river_lengths = {}
+        for river_name in river_names:
+            river_segments = self.rivers[self.rivers.river_name == river_name]
+            river_length = river_segments.length.sum()
+            print(f"\t{river_name}: {river_length:.4f}")
+            river_lengths[river_name] = river_length
+        longest_river = max(river_lengths, key=river_lengths.get)
+        print(f"\nLongest river: {longest_river}\n")
+        # only use longest river to make REM
+        self.rivers = self.rivers[self.rivers.river_name == longest_river]
+        """
         self.make_river_shp()
-
         return
 
     def make_river_shp(self):
+        """Make list of OSM Way object geometries into a shapefile"""
+        self.river_shp = f'{self.dem_name}_rivers.shp'
         driver = ogr.GetDriverByName('Esri Shapefile')
-        ds = driver.CreateDataSource('rivers.shp')
-        layer = ds.CreateLayer('', None, ogr.wkbMultiLineString)
-        # Add one attribute
+        ds = driver.CreateDataSource(self.river_shp)
+        # create empty multiline geometry layer
+        layer = ds.CreateLayer('', self.proj, ogr.wkbMultiLineString)
+        # Add fields
         layer.CreateField(ogr.FieldDefn('name', ogr.OFTString))
+        layer.CreateField(ogr.FieldDefn('id', ogr.OFTInteger))
         defn = layer.GetLayerDefn()
-
-        ## If there are multiple geometries, put the "for" loop here
-        for way in self.rivers:
-            print(f"Making layer for {way}")
+        # populate layer with a feature for each natural waterway geometrye
+        for i, way in self.rivers.iterrows():
             # Create a new feature (attribute and geometry)
             feat = ogr.Feature(defn)
-            feat.SetField('name', way.name)
-
-            # Make a geometry, from Shapely object
-            geom = ogr.CreateGeometryFromWkb(way['geometry'][0].wkb)
+            # set feature attributes
+            feat.SetField('name', way.river_name)
+            feat.SetField('id', way.river_id)
+            # set feature geometry from Shapely object
+            geom = ogr.CreateGeometryFromWkb(way.geometry.wkb)
             feat.SetGeometry(geom)
-
+            # add feature to layer
             layer.CreateFeature(feat)
             feat = geom = None  # destroy these
-
         # Save and close everything
         ds = layer = feat = geom = None
+
+    def get_dem_coords(self):
+        """Get x, y coordinates of DEM raster pixels as array"""
+        print("Getting coordinates of DEM pixels.")
+        r = gdal.Open(self.dem, gdal.GA_ReadOnly)
+        band = r.GetRasterBand(1)
+        self.dem_array = band.ReadAsArray()
+        # ensure that nodata values become np.nans in array
+        self.nodata_val = band.GetNoDataValue()
+        if self.nodata_val:
+            self.dem_array = np.where(self.dem_array == self.nodata_val, np.nan, self.dem_array)
+        rows, cols = np.shape(self.dem_array)
+        # get dimensions of raster
+        # TODO: account for rotation?
+        (upper_left_x, x_size, x_rotation, upper_left_y, y_rotation, y_size) = r.GetGeoTransform()
+        self.cell_w, self.cell_h = x_size, y_size
+        min_x, max_x = sorted([upper_left_x, upper_left_x + x_size * cols])
+        min_y, max_y = sorted([upper_left_y, upper_left_y + y_size * rows])
+        self.extent = (min_x, min_y, max_x, max_y)
+        # make arrays with x, y indices
+        x_indices = np.array([range(cols)] * rows)
+        y_indices = np.array([[i] * cols for i in range(rows)])
+        # map indices to coords
+        self.xs_array = x_indices * x_size + upper_left_x + (x_size / 2)
+        self.ys_array = y_indices * y_size + upper_left_y + (y_size / 2)
+        return
+
+    def get_river_elev(self):
+        """Get DEM values along river centerline"""
+        print("Getting river elevation at DEM pixels.")
+        # gdal_rasterize centerline
+        centerline_ras = f"{self.dem_name}_centerline.tif"
+        extent = f"-te {' '.join(map(str, self.extent))}"
+        res = f"-tr {self.cell_w} {self.cell_h}"
+        cmd = f"gdal_rasterize -a id {extent} {res} {self.river_shp} {centerline_ras}"
+        subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
+        # raster to numpy array same shape as DEM
+        r = gdal.Open(centerline_ras, gdal.GA_ReadOnly)
+        self.centerline_array = r.GetRasterBand(1).ReadAsArray()
+        # TODO: identify river with most active pixels in DEM domain (use that one to make REM)
+        pixel_counts = {}
+        for river_name, id in self.river_ids.items():
+            pixel_count = len(self.centerline_array[self.centerline_array == id])
+            pixel_counts[id] = pixel_count
+        longest_river_id = max(pixel_counts, key=pixel_counts.get)
+        print(f"\nLongest river in domain: {[name for name, id in self.river_ids.items() if id == longest_river_id][0]}")
+        # redefine array with 1 for longest river, 0 otherwise
+        self.centerline_array = np.where(self.centerline_array == longest_river_id, 1, np.nan)
+        # use np.where to get DEM elevation and coordinates at active pixels
+        self.river_x_coords = self.xs_array[np.where(self.centerline_array == 1)]
+        self.river_y_coords = self.ys_array[np.where(self.centerline_array == 1)]
+        self.river_wses = self.dem_array[np.where(self.centerline_array == 1)]
+        return
 
     def interp_river_elev(self):
         """Interpolate elevation at river centerline across DEM extent."""
         print("\nInterpolating river elevation across DEM extent.")
+        # coords of sampled pixels along centerline
+        c_sampled = np.array([self.river_x_coords, self.river_y_coords]).T
+        # coords to interpolate over
+        c_interpolate = np.dstack(np.array([self.xs_array, self.ys_array])).reshape(-1, 2)
+        # create 2D tree
+        print("Constructing tree")
+        tree = KDTree(c_sampled)
+        # find k nearest neighbors
+        print("Querying tree")
+        # eps is fractional distance error tolerance on estimate of nearest neighbor distances, speeds up calculation
+        distances, indices = tree.query(c_interpolate, k=20, eps=0.1, n_jobs=4)
+        # interpolate (IDW with power = 1)
+        print("Making interpolated WSE array")
+        weights = 1 / distances
+        weights = weights / weights.sum(axis=1).reshape(-1, 1)
+        interpolated_values = (weights * self.river_wses[indices]).sum(axis=1)
+        self.wse_interp_array = interpolated_values.reshape(np.shape(self.dem_array))
+        # interpolation scheme causes divide by zero at sampled pixels, overwrite the nans to elevation values there
+        river_elev_array = np.where(self.centerline_array == 1, self.dem_array, np.nan)
+        self.wse_interp_array = np.where(np.isnan(self.wse_interp_array), river_elev_array, self.wse_interp_array)
         return
 
     def detrend_dem(self):
         """Subtract interpolated river elevation from DEM elevation to get REM"""
         print("\nDetrending DEM.")
-        return
+        self.rem_array = self.dem_array - self.wse_interp_array
+
+        # plot quantiles to help guide color mapping
+        import matplotlib.pyplot as plt
+        rem_vals = self.rem_array[~np.isnan(self.rem_array)]
+        plt.hist(rem_vals, bins=1000, histtype='step')
+        plt.show()
+
+        self.rem_ras = f"{self.dem_name}_REM.tif"
+        # make copy of DEM raster
+        r = gdal.Open(self.dem, gdal.GA_ReadOnly)
+        driver = gdal.GetDriverByName("GTiff")
+        rem = driver.CreateCopy(self.rem_ras, r, strict=0)
+        # fill with REM array
+        rem.GetRasterBand(1).WriteArray(self.rem_array)
+        return self.rem_ras
+
+    def make_colormap(self):
+        """
+        Make colormap for REM based on natural breakpoints
+
+        possible schemes:
+            - make color scale logarithmic: 0, 1, 10, 20 seems to work well?
+            - make breaks based on peaks in hypsograph
+                - normalize hypsograph based on number of pixels in centerline, percentiles corresponding to n times that pixel count
+        """
+        river_pixels = self.centerline_array.sum()
+
+
 
     def make_image_blend(self):
         """Blend REM with DEM hillshade to make pretty finished product"""
         print("\nBlending REM with hillshade.")
+        # make hillshade of original DEM
+        dem_viz = RasterViz(self.dem, out_ext=".tif", )
+        dem_viz.make_hillshade(multidirectional=True)
+        # make hillshdae color using hillshade from DEM and color-relief from REM
+        rem_viz = RasterViz(self.rem_ras, out_ext=".tif", make_png=True, make_kmz=True, docker_run=False)
+        rem_viz.hillshade_ras = dem_viz.hillshade_ras
+        rem_viz.make_hillshade_color(cmap='terrain')
         return
 
     def run(self):
         """Make pretty REM/hillshade blend from DEM"""
         self.get_river_centerline()
+        self.get_river_elev()
         self.interp_river_elev()
         self.detrend_dem()
         self.make_image_blend()
@@ -123,7 +266,7 @@ class REMMaker(object):
 
 
 if __name__ == "__main__":
-    dem = "./test_dems/sc_river.tin.tif"
+    dem = "./test_dems/yukon_flats.tif"
     rem_maker = REMMaker(dem=dem)
 
     end = time.time()
