83eaa5bb227a9c9bd9411c50f034239666a34471
Kenneth Larrieu <kennylarrieu@gmail.com>
2025-04-16 12:00:01 -0700

    bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ autodoc_mock_imports = ['gdal']
 project = 'RiverREM'
 copyright = '2025, Kenneth Larrieu'
 author = 'Kenneth Larrieu'
-release = '1.1.1'
+release = '1.1.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="RiverREM",
-    version="1.1.1",
+    version="1.1.2",
     author="Kenneth Larrieu",
     author_email="kennylarrieu@gmail.com",
     description="Make river relative elevation models (REM) and REM visualizations from an input digital elevation model (DEM).",
